1bafc4cb7d821517a5b4b0e340854fcfc5015613
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2025-09-04 13:28:07 +1000

    feat: add About page styles and structure; implement responsive design for content sections

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,8 @@
 import { urlFor } from "@/sanity/lib/image";
 import { getAbout } from "@/sanity/sanity.utils";
 import Image from "next/image";
+import "./About.css";
+import "@/components/Grid.css";
 
 export const revalidate = 5;
 
@@ -8,36 +10,42 @@ export default async function About() {
   const aboutInformation = await getAbout();
 
   return (
-    <main>
-      <div>
-        <h2>ABOUT</h2>
-        <p>{aboutInformation?.description}</p>
+    <main className="about-page mobile-padding grid">
+      <div className="about-page-image spacing-24">
+        {aboutInformation?.aboutImage?.asset?.url && (
+          <Image
+            src={urlFor(aboutInformation?.aboutImage)
+              .auto("format")
+              .quality(85)
+              .url()}
+            width={800}
+            height={1600}
+            alt={aboutInformation?.aboutImage.alt || ""}
+            className="about-page-img"
+            priority={false}
+          />
+        )}
       </div>
-      <div>
-        <h2>CONTACT</h2>
-        <p>{aboutInformation?.email}</p>
-        {aboutInformation?.aboutSocialLinks?.map((link, index) => (
-          <div key={index}>
-            <a href={link?.url || ""} target="_blank" rel="noopener noreferrer">
-              {link?.platform}
-            </a>
-          </div>
-        ))}
-      </div>
-      {aboutInformation?.aboutImage?.asset?.url && (
-        <Image
-          src={urlFor(aboutInformation?.aboutImage)
-            .width(3840)
-            .auto("format")
-            .quality(85)
-            .url()}
-          width={800}
-          height={1600}
-          alt={aboutInformation?.aboutImage.alt || ""}
-          className="header-media-img"
-          priority={false}
-        />
-      )}
+      <h2 className="about-heading type-body-bold">ABOUT</h2>
+      <p className="about-body-text spacing-32 type-body">
+        {aboutInformation?.description}
+      </p>
+      <h2 className="about-contact-heading type-body-bold">CONTACT</h2>
+      <p className="about-contact-body-text spacing-24 type-body">
+        {aboutInformation?.email}
+      </p>
+      {aboutInformation?.aboutSocialLinks?.map((link, index) => (
+        <div key={index} className="about-page-social-links">
+          <a
+            href={link?.url || ""}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="about-page-social-links spacing-32 type-body"
+          >
+            {link?.platform}
+          </a>
+        </div>
+      ))}
     </main>
   );
 }
