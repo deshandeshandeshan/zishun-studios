805277fa82e312a1589f0810bbd66440117eec08
deshandeshandeshan <deshan.mclachlan0@gmail.com>
2025-09-04 14:26:27 +1000

    feat: add styles and structure for paintings and selected works pages; implement responsive design for grid layout and items

--- app/selected-works/page.tsx
+++ app/selected-works/page.tsx
@@ -2,15 +2,16 @@ import { urlFor } from "@/sanity/lib/image";
 import { getSelectedWorks } from "@/sanity/sanity.utils";
 import Image from "next/image";
 import Link from "next/link";
+import "./SelectedWorks.css";
 
 export default async function SelectedWorks() {
   const selectedWorks = await getSelectedWorks();
 
   return (
-    <main>
-      <ul>
+    <main className="selected-works-page">
+      <ul className="selected-works-list selected-works-grid">
         {selectedWorks.map((work) => (
-          <li key={work._id}>
+          <li key={work._id} className="selected-work-item">
             {work.selectedWorkImage?.asset?.url && (
               <Image
                 src={urlFor(work.selectedWorkImage?.asset?.url)
@@ -20,12 +21,21 @@ export default async function SelectedWorks() {
                 alt={work.selectedWorkImage?.alt || ""}
                 width={2160}
                 height={3840}
-                className=""
+                className="selected-work-image"
               />
             )}
-            <h2>{work.title}</h2>
-            <p>{work.role}</p>
-            <Link href={`/selected-works/${work.slug}`}>See more</Link>
+            <div className="selected-work-info">
+              <h2 className="selected-work-title type-body-bold">
+                {work.title}
+              </h2>
+              <p className="type-body selected-work-role">{work.role}</p>
+              <Link
+                href={`/selected-works/${work.slug}`}
+                className="type-body selected-work-link"
+              >
+                See more &rarr;
+              </Link>
+            </div>
           </li>
         ))}
       </ul>
